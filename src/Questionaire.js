62303d946ac870237c0113ebc2091e01acbc3282
demetriam <demetriamack04@gmail.com>
2021-12-06 20:10:53 -0500

    styling to questionaire

---
+++ src/Questionaire.js
@@ -0,0 +1,15 @@
+import './Questionaire.css'
+import './Questionaire.html'
+import {Link} from 'react-router-dom';
+
+function Questionaire() {
+    return(
+        <div className= "Background">
+            <div className= "Header">
+                <h1 className= "Title"> Meet The Herd </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Questionaire;
