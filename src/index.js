654f704a65ff18e2ab8e36e3812956aac795e961
byeolhoshi <asmith@karat.com>
2021-12-12 13:17:15 -0600

    23 installing dependencies

--- src/index.js
+++ src/index.js
@@ -6,152 +6,13 @@
  import React from 'react';
   import ReactDOM from 'react-dom';
   import './index.css';
-  import reportWebVitals from './reportWebVitals';
   import ReturningUserProfile from './ReturningUserProfile';
   import NewUserProfile from './NewUserProfile';
   import { BrowserRouter, Routes, Route } from 'react-router-dom';
   import HomePage from './HomePage';
 
-
-  import { initializeApp } from 'firebase/app';
-  import {
-   getAuth,
-   onAuthStateChanged,
-   GoogleAuthProvider,
-   signInWithPopup,
-   signOut,
- } from 'firebase/auth';
-/*  import {
-   getFirestore,
-   collection,
-   addDoc,
-   query,
-   orderBy,
-   limit,
-   onSnapshot,
-   setDoc,
-   updateDoc,
-   doc,
-   serverTimestamp,
- } from 'firebase/firestore';
-/*  import {
-   getStorage,
-   ref,
-   uploadBytesResumable,
-   getDownloadURL,
- } from 'firebase/storage'; */
-
- ReactDOM.render(
-  <BrowserRouter>
-      <Routes>
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/" element={<NewUserProfile />} />
-        <Route path="/returnUserProfile" element={<ReturningUserProfile />} />
-      </Routes>
- </BrowserRouter>,
-  document.getElementById('root')
-);
-
- import { getPerformance } from 'firebase/performance';
- 
- import { getFirebaseConfig } from './firebase-config.js';
- import SignIn from './signin';
- 
- // Signs-in Friendly Chat.
- async function signIn() {
-   // Sign in Firebase using popup auth and Google as the identity provider.
-   var provider = new GoogleAuthProvider();
-   await signInWithPopup(getAuth(), provider);
- }
- 
- // Signs-out of Friendly Chat.
- function signOutUser() {
-   // Sign out of Firebase.
-   signOut(getAuth());
- }
- 
- // Initiate firebase auth
- function initFirebaseAuth() {
-   // Listen to auth state changes.
-   onAuthStateChanged(getAuth(), authStateObserver);
- }
- 
- // Returns the signed-in user's profile Pic URL.
- function getProfilePicUrl() {
-   return getAuth().currentUser.photoURL || '/images/profile_placeholder.png';
- }
- 
- // Returns the signed-in user's display name.
- function getUserName() {
-   return getAuth().currentUser.displayName;
- }
- 
- // Returns true if a user is signed-in.
- function isUserSignedIn() {
-   return !!getAuth().currentUser;
- }
- 
- function authStateObserver(user) {
-   if (user) {
-     // User is signed in!
-     // Get the signed-in user's profile pic and name.
-     var profilePicUrl = getProfilePicUrl();
-     var userName = getUserName();
- 
-     // Set the user's profile pic and name.
-     userPicElement.style.backgroundImage =
-       'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
-     userNameElement.textContent = userName;
- 
-     // Show user's profile and sign-out button.
-     userNameElement.removeAttribute('hidden');
-     userPicElement.removeAttribute('hidden');
-     signOutButtonElement.removeAttribute('hidden');
- 
-     // Hide sign-in button.
-     signInButtonElement.setAttribute('hidden', 'true');
- 
-     // We save the Firebase Messaging Device token and enable notifications.
-     saveMessagingDeviceToken();
-   } else {
-     // User is signed out!
-     // Hide user's profile and sign-out button.
-     userNameElement.setAttribute('hidden', 'true');
-     userPicElement.setAttribute('hidden', 'true');
-     signOutButtonElement.setAttribute('hidden', 'true');
- 
-     // Show sign-in button.
-     signInButtonElement.removeAttribute('hidden');
-   }
- }
- 
- // Returns true if user is signed-in. Otherwise false and displays a message.
- // A loading image URL.
  var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
  
- 
- // A loading image URL.
- var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
- 
- /* function toggleButton() {
-   if (messageInputElement.value) {
-     submitButtonElement.removeAttribute('disabled');
-   } else {
-     submitButtonElement.setAttribute('disabled', 'true');
-   }
- }
- 
- // A loading image URL.
- var LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif?a';
- 
- // Shortcuts to DOM Elements.
-/*  var messageListElement = document.getElementById('messages');
- var messageFormElement = document.getElementById('message-form');
- var messageInputElement = document.getElementById('message');
- var submitButtonElement = document.getElementById('submit');
- var imageButtonElement = document.getElementById('submitImage');
- var imageFormElement = document.getElementById('image-form');
- var mediaCaptureElement = document.getElementById('mediaCapture'); */
  var userPicElement = document.getElementById('user-pic');
  var userNameElement = document.getElementById('user-name');
  var signInButtonElement = document.getElementById('sign-in');
@@ -161,8 +22,7 @@
  const firebaseAppConfig = getFirebaseConfig();
  
  if (isUserSignedIn()){
-   return (
-     ReactDOM.render(
+    ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
@@ -174,9 +34,8 @@
     </React.StrictMode>,
   document.getElementById('root')
 )
-   )
  }else{
-   return(
+   ReactDOM.render(
      <React.StrictMode>
        <BrowserRouter>
         <Router>
@@ -188,32 +47,7 @@
      </React.StrictMode>
    )
  }
- 
- reportWebVitals();
- // TODO 12: Initialize Firebase Performance Monitoring
- getPerformance();
- initializeApp(firebaseAppConfig);
- initFirebaseAuth();
- loadMessages();
- initFirebaseAuth();
- reportWebVitals();
- reportWebVitals();
- 
- const firebaseAppConfig = getFirebaseConfig();
- 
- // TODO 12: Initialize Firebase Performance Monitoring
- ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/" element={<NewUserProfile />} />
-        <Route path="/returnUserProfile" element={<ReturningUserProfile />} />
-      </Routes>
-  </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
  reportWebVitals();
  initializeApp(firebaseAppConfig);
  initFirebaseAuth();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   // The entry point file described above
+
   entry: './src/index.js',
   // The location of the build folder described above
   output: {
@@ -18,14 +19,14 @@ module.exports = {
                   options:{
                       cacheDirectory: true,
                       cacheCompression: false, 
-                      envName: isProduction ? "production" : "development"
+                      envName: "development"
                   }
               }
           },
           {
               test: /\.css$/,
               use: [
-                  isProduction ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"
+                  loader = "css-loader"
               ]
           }
       ]
@@ -33,13 +34,6 @@ module.exports = {
   resolve: {
       extensions: [".js", ".jsx"]
   },
-  plugins: [
-      isProduction &&
-        new MiniCssExtractPlugin({
-            filename: "assets/css/[name].[contenthash:8].css"
-            chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
-        })
-  ].filter(Boolean),
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
   devtool: 'eval-source-map',
