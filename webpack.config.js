a0bff8bc2b966194c7137dc33d3dde8fd03dae7a
byeolhoshi <asmith@karat.com>
2021-12-12 14:08:06 -0600

    23 webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,58 @@
+//
+//https://webpack.js.org/configuration/configuration-types/
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+module.exports = funcion(_env, argv) {
   // The entry point file described above
+  const isProduction = argv.mode === "production";
+  const isDevelopment = !isProduction; 
+  return{
+        entry: './src/index.js',
+        // The location of the build folder described above
+        output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "../public",
+        devtool: isDevelopmeent && 'cheap-module-source-map'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use:{
+                        loader: "babel-loader", 
+                        options:{
+                            cacheDirectory: true,
+                            cacheCompression: false, 
+                            envName: isProduction ? "production" : "development"
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader"
+                    ]
+                },
 
-  entry: './src/index.js',
-  // The location of the build folder described above
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-      rules: [
-          {
-              test: /\.jsx?$/,
-              exclude: /node_modules/,
-              use:{
-                  loader: "babel-loader", 
-                  options:{
-                      cacheDirectory: true,
-                      cacheCompression: false, 
-                      envName: "development"
-                  }
-              }
-          },
-          {
-              test: /\.css$/,
-              use: [
-                  loader = "css-loader"
-              ]
-          }
-      ]
-  }, 
-  resolve: {
-      extensions: [".js", ".jsx"]
-  },
-  // Optional and for development only. This provides the ability to
-  // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+            ]
+        }, 
+        resolve: {
+            extensions: [".js", ".jsx"]
+    },
+        plugins: [
+            isProduction && 
+                new MiniCssExtractPlugin({
+                    filename: "assets/css/[name].[contenthash:8].css",
+                    chunkFilename: "assets/css/[name]/[contenthash:8].chunk.css"
+                })
+            ].filter(Boolean)
+        // Optional and for development only. This provides the ability to
+        // map the built code back to the original source format when debugging.
+        
+  }
   
 };
