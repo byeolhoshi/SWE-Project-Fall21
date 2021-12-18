5ef49022462213f6da4da0ad7f6f03a430a2622b
Iceman272 <72137901+Iceman272@users.noreply.github.com>
2021-12-18 15:06:31 -0500

    Create SortingHat.js
    To match a user to club after quiz using their [answers] and a 2D array that stores all the [clubAnswers]

---
+++ public/SortingHat.js
@@ -0,0 +1,38 @@
+
+function ListSorter(x, answers, clubAnswers, solutions){
+	
+	let temp=0;
+
+	for (let y =0; y < grid[0].length; y++)
+	{
+		if(answers[x][y]==clubAnswers[x][y])
+		{
+			temp++;
+
+		}
+	}
+	solutions[x]=temp;
+}
+
+exports.findClub = function(answers, clubAnswers) {
+	let solutions = [];
+	let output = 0;
+	
+	if (clubAnswers == null || answers == null){return output;}
+
+	for(let x =0; x < clubAnswers.length; x++)
+	{
+		ListSorter(x, answers, clubAnswers, solutions);
+
+	}
+
+	let max=0;
+	for(let c=0; c<solutions.length;c++)
+	{
+		if(solutions[c] > solutions[max])
+		{max=c;}
+	}
+	output=max;
+
+	return output;
+};
